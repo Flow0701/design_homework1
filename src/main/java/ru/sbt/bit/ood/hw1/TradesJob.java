30e0b47d38765086d67e9f47291cacc8bec6ec40
Stratilov <Stratilov>
2016-12-17 06:15:44 +0400

    Initial commit

---
+++ src/main/java/ru/sbt/bit/ood/hw1/TradesJob.java
@@ -0,0 +1,28 @@
+package ru.sbt.bit.ood.hw1;
+
+import org.apache.commons.csv.CSVRecord;
+
+public class TradesJob {
+
+    private final TradesDAO tradesDAO;
+
+    public TradesJob(TradesDAO tradesDAO) {
+        this.tradesDAO = tradesDAO;
+    }
+
+    public void run() {
+        String filename = DownloaderFromFTP.downloadTradesFileFromFTP();
+        Iterable<CSVRecord> tradeRecords = ParcerCSV.parse(filename);
+        updateTrades(tradeRecords);
+    }
+
+    private void updateTrades(Iterable<CSVRecord> trades) {
+        tradesDAO.deleteAll();
+        for (CSVRecord tradeRecord : trades) {
+            Trade trade = new Trade(tradeRecord.toMap());
+            tradesDAO.save(trade);
+        }
+    }
+
+
+}
